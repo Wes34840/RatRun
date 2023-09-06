6f11d678e278dbcd0e8b0253fb6216b7e761842a
Wes <114735316+Wes34840@users.noreply.github.com>
2023-09-06 12:31:21 +0200

    mid-development commit

--- RatRun/Assets/WesScripts/MapGen/GenerateMap.cs
+++ RatRun/Assets/WesScripts/MapGen/GenerateMap.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 public class GenerateMap : MonoBehaviour
 {
-    [SerializeField] private Sprite laneSprites;
+    [SerializeField] private Sprite[] laneSprites;
     [SerializeField] private GameObject lanePrefab;
     private GameObject[] lanes = new GameObject[12];
     
     private void Start()
     {
-        var laneWidth = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
-        var laneHeight = Camera.main.orthographicSize * 2.0 / 12;
+        var screenWidth = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height / 19.2;
+        var screenHeight = Camera.main.orthographicSize * 2.0;
+        var laneHeight = screenHeight / 12;
 
+        LaneType[] mapLayout = new LaneType[]
+        {
+                LaneType.Grass,
+                LaneType.Road,
+                LaneType.Road,
+                LaneType.Road,
+                LaneType.Road,
+                LaneType.Grass,
+                LaneType.Water,
+                LaneType.Water,
+                LaneType.Water,
+                LaneType.Water,
+                LaneType.Grass
+        }; //bandaid fix because busy with fixing sprites
 
         for (int i = 0; i < lanes.Length; i++)
         {
-            lanes[i] = Instantiate(lanePrefab, new Vector3(transform.position.x, -Camera.main.orthographicSize + ((float)laneHeight * i) + ((float)laneHeight/2),  transform.position.z), Quaternion.identity);
+            lanes[i] = Instantiate(lanePrefab, new Vector3(transform.position.x, -Camera.main.orthographicSize + ((float)laneHeight * i) + ((float)laneHeight / 2),  transform.position.z), Quaternion.identity);
 
-            lanes[i].transform.localScale = new Vector3((float)laneWidth, (float)laneHeight, 0);
+            lanes[i].GetComponent<Lane>().type = mapLayout[i];
+
+            lanes[i].transform.localScale = new Vector3((float)screenWidth, (float)laneHeight, 0);
 
             RenderLane(lanes[i]);
+
+            lanes[i].transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 1.2f, 0);
         }
     }
     private void RenderLane(GameObject lane)
     {
-        //lane.GetComponent<SpriteRenderer>().sprite = laneSprites[lane.GetComponent<Lane>().LaneType]
+        SpriteRenderer render = lane.GetComponent<SpriteRenderer>();
+        render.sprite = laneSprites[(int)lane.GetComponent<Lane>().type];
     }
 }
